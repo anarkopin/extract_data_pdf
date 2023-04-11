0e1715cb98edab0dce1474e553b93970427c2312
Diego Bejar <diebejardelaguila@gmail.com>
2023-04-11 08:09:09 -0500

    Resolve debug error 3

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 JWT_SECRET = os.environ.get('JWT_SECRET')
 
-DEBUG= os.environ.get('DEBUG')
+DEBUG= True
 
 # ALLOWED_HOSTS = env.list('ALLOWED_HOSTS_DEV')
 
